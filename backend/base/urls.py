7dac81ecb68251cef3cff75e362b53f1af311e6d
Abella <neabella2@student.hau.edu.ph>
2026-03-05 20:39:51 +0800

    fix: re-pathed 'signin/'

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 urlpatterns = [
     path('', views.getRoutes, name='routes'),
     path('signup/', views.register_view, name='signup'),
-    path('signin/', MyTokenObtainPairView.as_view(), name='signin'),
-    path('conversation/', views.chat_view, name="conversation"),
+    path('signin/', views.signin_view, name='signin'),
+    path('conversation/', views.conversation, name="conversation"),
     path('conversations/', views.conversation_list_view, name="conversation-list"),
     path('conversations/<str:pk>/', views.conversation_detail_view, name="conversation-detail"),
 ]
