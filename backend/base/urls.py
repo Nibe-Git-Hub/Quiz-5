b65dd42c2474bf518f3180349631b72fea482760
Abella <neabella2@student.hau.edu.ph>
2026-03-05 19:16:12 +0800

    feat: convo views and signin and signup paths

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views 
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('', views.getRoutes, name='routes'),
-    path('chat/', views.gemini, name="chat"),
+    path('signup/', views.register_view, name='signup'),
+    path('signin/', MyTokenObtainPairView.as_view(), name='signin'),
+    path('conversation/', views.chat_view, name="conversation"),
+    path('conversations/', views.conversation_list_view, name="conversation-list"),
+    path('conversations/<str:pk>/', views.conversation_detail_view, name="conversation-detail"),
 ]
